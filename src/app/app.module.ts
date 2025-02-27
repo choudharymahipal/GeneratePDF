341d0101e24976eb2c2c55bc3fbfaa2bd40a6ef9
choudharymahipal <mschoudhary81@gmail.com>
2025-02-27 16:59:22 +0530

    boostrap added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { SampleComponent } from './Components/sample/sample.component';
 import { HomeComponent } from './home/home.component';
 import { ImageComponent } from './Components/image/image.component';
+import { HeaderComponent } from './Shared/Components/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SampleComponent,
     HomeComponent,
-    ImageComponent
+    ImageComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
